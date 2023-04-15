3f66a64bbb872037546d3ccb6f167d7518fa651f
DeGaMon <quentin.gchnk@gmail.com>
2023-04-15 14:19:19 +0200

    Update wifi_ap.sh

--- docs/base/wifi_ap.sh
+++ docs/base/wifi_ap.sh
@@ -2,16 +2,16 @@
 echo "### Install AP and Management Software"
 # Install and set up hostapd package
 echo "# Install and set up hostapd service"
-sudo apt update
-sudo apt install hostapd
-sudo systemctl unmask hostapd
-sudo systemctl enable hostapd
+sudo apt update	&&
+sudo apt install hostapd &&
+sudo systemctl unmask hostapd &&
+sudo systemctl enable hostapd &&
 # Install dnsmasq package
 echo "# Install dnsmasq package"
-sudo apt install dnsmasq
+sudo apt install dnsmasq &&
 # Install netfilter and iptables (firewall) pacakges
 echo "# Install netfilter and iptables (firewall) pacakges"
-sudo DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent
+sudo DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent &&
 
 ### Set up the Network Router
 echo "### Set up the Network Router"
@@ -22,13 +22,13 @@ sudo echo "
 interface wlan0
 static ip_address=192.168.4.1/24
 nohook wpa_supplicant
-" | sudo tee -a /etc/dhcpcd.conf
+" | sudo tee -a /etc/dhcpcd.conf &&
 echo "to /etc/dhcpcd.conf... Done."
 # Skipping "Enable routing and IP Masquerading" step
 echo "# Skipping 'Enable routing and IP Masquerading' step"
 # Configure the DHCP and DNS services for the wireless network
 echo "# Configure the DHCP and DNS services for the wireless network"
-sudo mv /ect/dnsmasq.conf /etc/dnsmasq.conf.orig
+sudo mv /ect/dnsmasq.conf /etc/dnsmasq.conf.orig &&
 echo "Writing:"
 sudo echo "
 # Listening interface
@@ -39,7 +39,7 @@ dhcp-range=192.168.4.2,192.168.4.20,255.255.255.0,24h
 domain=wlan     
 # Alias for this router
 address=/basegnss.wlan/192.168.4.1
-"| sudo tee /etc/dnsmasq.conf
+"| sudo tee /etc/dnsmasq.conf &&
 echo "to /etc/dnsmasq.conf... Done."
 
 ### Ensure Wireless Operation
@@ -63,5 +63,5 @@ wpa_passphrase=basegnss!
 wpa_key_mgmt=WPA-PSK
 wpa_pairwise=TKIP
 rsn_pairwise=CCMP
-" | sudo tee /etc/hostapd/hostapd.conf
+" | sudo tee /etc/hostapd/hostapd.conf &&
 echo "to /etc/hostapd/hostapd.conf"
